08cb3953de313303448487f11ce544746fc27a0f
k-toyoda <toyoda@combine.co.jp>
2023-09-11 12:53:26 +0900

    Add `handleUnknownError()`

--- src/common/errors.ts
+++ src/common/errors.ts
@@ -15,3 +15,19 @@ export class BaseError extends Error {
     Object.setPrototypeOf(this, new.target.prototype); // Necessary for ES6 class inheritance
   }
 }
+
+/**
+ * Converts an unknown error into an Error instance with a descriptive message.
+ *
+ * @param error - The unknown error to handle.
+ * @returns An Error instance with a descriptive message.
+ */
+export function handleUnknownError(error: unknown): Error {
+  if (error instanceof Error) {
+    return error;
+  } else if (typeof error === "string") {
+    return new Error(`Unknown error: ${error}`);
+  } else {
+    return new Error("An unexpected error occurred");
+  }
+}
