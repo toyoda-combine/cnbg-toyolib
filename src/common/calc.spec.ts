46a089f5434bed0c864bb2e724d80c0779c5a568
k-toyoda <toyoda@combine.co.jp>
2023-09-26 18:14:05 +0900

    Add average function

--- src/common/calc.spec.ts
+++ src/common/calc.spec.ts
@@ -1,4 +1,4 @@
-import { sum } from "./calc";
+import { sum, average } from "./calc";
 
 describe("Tests for sum function", () => {
   it.each`
@@ -13,6 +13,21 @@ describe("Tests for sum function", () => {
     ${[Number.MAX_VALUE, Number.MIN_VALUE]} | ${Number.MAX_VALUE + Number.MIN_VALUE} | ${"Sum of max and min floating-point numbers"}
   `("$description", ({ array, expected }) => {
     const actual = sum(array);
+    expect(actual).toBe(expected);
+  });
+});
+
+describe("average function", () => {
+  it.each`
+    array           | expected | description
+    ${[1, 2, 3]}    | ${2}     | ${"calculates the average of a positive array"}
+    ${[-1, 0, 1]}   | ${0}     | ${"calculates the average of a mixed array"}
+    ${[100, 200]}   | ${150}   | ${"calculates the average of a high-valued array"}
+    ${[0, 0, 0]}    | ${0}     | ${"calculates the average of an array with all zeros"}
+    ${[-1, -2, -3]} | ${-2}    | ${"calculates the average of a negative array"}
+    ${[]}           | ${null}  | ${"returns null for an empty array"}
+  `("$description", ({ array, expected }) => {
+    const actual = average(array);
     expect(actual).toBe(expected);
   });
 });
--- src/common/calc.ts
+++ src/common/calc.ts
@@ -7,3 +7,15 @@
 export function sum(array: number[]): number {
   return array.reduce((acc, curr) => acc + curr, 0);
 }
+
+/**
+ * Calculates the average of an array of numbers.
+ * @param array - The array of numbers to calculate the average for.
+ * @returns The average of the array of numbers. Returns null if the array is empty.
+ */
+export function average(array: number[]): number | null {
+  if (array.length === 0) {
+    return null;
+  }
+  return sum(array) / array.length;
+}
